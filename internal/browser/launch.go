c1b52f5579f3733134a794c4019fc65b730cd623
Eric Fixler <efixler@mozilla.com>
2024-02-28 22:41:27 -0500

    Get status code when navigating

--- internal/browser/launch.go
+++ internal/browser/launch.go
@@ -26,34 +26,49 @@ func (b *Browser) HTMLContent(url string) (string, error) {
 	ctx, cancel := chromedp.NewContext(b.ctx)
 	defer cancel()
 
-	var htmlContent string
+	var html string
 	var statusCode int
-	if err := chromedp.Run(ctx,
+
+	listenCtx, cancelListen := context.WithCancel(ctx)
+	chromedp.ListenTarget(listenCtx, func(ev interface{}) {
+		if res, ok := ev.(*network.EventResponseReceived); ok {
+			// see https://chromedevtools.github.io/devtools-protocol/tot/Network/#type-Response
+			// eventChan <- res
+			statusCode = int(res.Response.Status)
+			slog.Info("Received response",
+				"url", res.Response.URL,
+				"type", res.Type,
+				"status", statusCode,
+				"statusText", res.Response.StatusText,
+			)
+			cancelListen()
+		}
+	})
+	slog.Debug("Listening for response", "url", url)
+
+	err := chromedp.Run(ctx,
 		chromedp.Navigate(url),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			chromedp.ListenTarget(ctx, func(ev interface{}) {
-				switch ev := ev.(type) {
-				case *network.EventResponseReceived:
-					statusCode = int(ev.Response.Status)
-					slog.Info("Received response", "status", statusCode)
-				}
-			})
-			return nil
-		}),
+		chromedp.Sleep(1*time.Second),
 		chromedp.WaitReady("body"),
-		chromedp.Sleep(2*time.Second),
-		chromedp.OuterHTML("html", &htmlContent),
-	); err != nil {
-		return "", err
+		chromedp.OuterHTML("html", &html),
+	)
+
+	slog.Debug("Run completed HTML content", "url", url)
+	if err != nil {
+		// see https://github.com/chromedp/chromedp/blob/ebf842c7bc28db77d0bf4d757f5948d769d0866f/nav.go#L26
+		// bad domain = page load error net::ERR_NAME_NOT_RESOLVED
+		slog.Error("Error getting HTML content", "url", url, "err", err)
 	}
-	return htmlContent, nil
+
+	return html, err
 }
 
 func getAllocatorOptions() []chromedp.ExecAllocatorOption {
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		// chromedp.Flag("headless", true),
+		// chromedp.Flag("headless", false),
 		// chromedp.Flag("disable-gpu", true),
 		chromedp.Flag("disable-dev-shm-usage", true),
+		chromedp.Flag("blink-settings", "imagesEnabled=false"),
 		// chromedp.Flag("mute-audio", true), // included in Headless
 		// chromedp.Flag("remote-debugging-address", "127.0.0.1"),
 		// chromedp.Flag("remote-debugging-port", fmt.Sprintf("%d", 9222)),
